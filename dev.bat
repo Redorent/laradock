859a0d1c412ad6ec2a2f02464923b31715fdefad
Jarko Vihriala <jarko@vihriala.fi>
2018-03-16 01:31:49 +0200

    imrovements to dev tools

--- dev.bat
+++ dev.bat
@@ -3,13 +3,23 @@
 if "%1" == "start" goto start
 if "%1" == "end" goto shutdown
 if "%1" == "workspace" goto workspace
+if "%1" == "mysql" goto mysql
+if "%1" == "mysql-shell" goto mysql-shell
 
+goto end
 
+:mysql
+call docker-compose exec mysql mysql -uroot -proot "%2"
+goto end
+
+:mysql-shell
+call docker-compose exec mysql bash
 goto end
 
 
 :workspace
 call docker-compose exec --u=laradock workspace bash
+goto end
 
 :start
 call docker-compose up -d mysql nginx phpmyadmin
