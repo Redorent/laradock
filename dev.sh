f6907a43e12405a875a6b7a563908d4435652d9b
jarko vihriala <jarko.vihriala@redorent.fi>
2018-06-24 17:46:38 +0300

    support for 'end' function on Mac

--- dev.sh
+++ dev.sh
@@ -6,6 +6,12 @@ if [ "$1" == "start" ];
 	exit
 fi
 
+if [ "$1" == "end" ];
+	then
+		docker-compose down
+		exit
+fi
+
 if [ "$1" == "workspace" ];
 	then
 	docker-compose exec --u=laradock workspace bash
