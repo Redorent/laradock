8e08d823ebf747dc923ebb6f4e0a9dc8b2419051
Jarko Vihriaelae <jarko.vihriala@redorent.fi>
2019-04-24 14:57:38 +0300

    dev.sh update

--- dev.sh
+++ dev.sh
@@ -4,22 +4,20 @@ case "$1" in
 start*)
 	export DATA_PATH_HOST=$HOME/.laradock/data
 	export MONGO_DATA_PATH=$DATA_PATH_HOST
-	docker-compose up -d --build phpmyadmin mysql nginx mongo
+	docker-compose up -d
 	if [ "$?" -eq 0 ];
 	then
 		docker-compose exec --u=laradock workspace bash
 	fi 
 	;;
 end*)
-	docker-compose down
-	;;
-mongo*)
-	docker-compose exec mongo mongo
+	docker-compose down	
 	;;
 workspace*)
 	docker-compose exec --u=laradock workspace bash
 	;;
 *)
+	docker-compose exec "$1" bash
 	echo $"Usage: $0 {start|end|mongo|workspace}"
 	exit 1
 esac
