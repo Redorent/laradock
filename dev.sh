809128888a8a6bed27a977668a0ccf3c1ddb811e
Jarko Vihriaelae <jvihrial@gmail.com>
2019-06-25 11:07:05 +0300

    simplified dev.sh

--- dev.sh
+++ dev.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 
 case "$1" in
-start*)
-	export DATA_PATH_HOST=$HOME/.laradock/data
-	export MONGO_DATA_PATH=$DATA_PATH_HOST
-	docker-compose up -d
-	if [ "$?" -eq 0 ];
-	then
-		docker-compose exec --u=laradock workspace bash
-	fi 
-	;;
 end*)
 	docker-compose down	
-	;;
-workspace*)
-	docker-compose exec --u=laradock workspace bash
-	;;
+	;;	
 *)
-	docker-compose exec "$1" bash
-	echo $"Usage: $0 {start|end|mongo|workspace}"
-	exit 1
+	IS_RUNNING=$(docker-compose ps | grep Up | grep workspace | wc -l)
+	
+	if [ "$IS_RUNNING" -eq 1 ];
+	then
+		docker-compose exec --u=laradock workspace bash
+	else 
+		export DATA_PATH_HOST=$HOME/.laradock/data
+		export MONGO_DATA_PATH=$DATA_PATH_HOST
+		docker-compose up -d
+		if [ "$?" -eq 0 ];
+		then
+			docker-compose exec --u=laradock workspace bash
+		fi 
+	fi
 esac
 
 exit
